c99d7336073a3cf4e9be7364c728b1eb9d5e96bb
Asha-2024 <ashaaws2024@gmail.com>
2025-01-27 22:29:59 +0530

    added 2nd script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -4,6 +4,22 @@ ID=$(id -u)
 Rm"\e[31m"
 Gm"\e[32m"
 Nm"\e[0m"
+Ym"\e[33m"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+VALIDATE(){
+    if[ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
 
 if [ $ID -ne 0 ] 
 then
@@ -14,3 +30,17 @@ then
     fi # fi means reverse of if, indicating conditi
 
     echo "All arguments passed: $@"
+# git mysql postfix net.tools
+# package=git for first time
+
+    for package in $@ 
+    do
+        yum list installed $package &>> $LOGFILE #check installed or not
+        if [ $? -ne 0 ] #if not installed
+        then
+            yum install $package -y &>> $LOGFILE # install the package
+            VALIDATE $? "Installation of $package" validate
+        else
+        echo -e"$package is already installed ... $Y SKIPPING $N"
+    
+    done
